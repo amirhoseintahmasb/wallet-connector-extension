cf48661f7c9eb3b065255284954112ea7555ea7b
alipoudine <ali.poudine99@gmail.com>
2024-05-02 00:43:36 +0330

    new version

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,16 @@ const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const log = require('gulplog');
-const gulpESLintNew = require('gulp-eslint-new');
 const connect = require('gulp-connect');
 const sourcemaps = require('gulp-sourcemaps');
 
 const config = {
-  name: 'wallet connector extention',
+  name: 'Wallet Connector Extention',
   root: 'dist',
-  port: 3001,
-  devBaseUrl: 'http://localhost',
   paths: {
     js: './src/**/*.js',
   }
 };
-
-gulp.task('lint', function () {
-  return gulp.src(config.paths.js)
-    .pipe(gulpESLintNew())
-    .pipe(gulpESLintNew.format())
-    .pipe(gulpESLintNew.failAfterError());
-});
 
 gulp.task('build', function () {
   return browserify({ entries: './src/index.js', debug: true })
@@ -33,20 +23,8 @@ gulp.task('build', function () {
     .pipe(source('wallet-connector-extention.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.root))
     .pipe(connect.reload());
-});
-
-gulp.task('connect', function (done) {
-  connect.server({
-    name: 'wallet-connector-extention',
-    root: config.root,
-    port: config.port,
-    livereload: true
-  });
-  require('open')(config.devBaseUrl + ':' + config.port);
-  done();
 });
 
 gulp.task('default', gulp.series('build'));
