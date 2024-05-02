9acb85fac8e54c526d8c6c74770a9bb2c104ef2f
alipoudine <ali.poudine99@gmail.com>
2024-05-02 05:40:11 +0330

    new version

--- src/index.js
+++ src/index.js
@@ -11,173 +11,19 @@ if (typeof process === 'undefined') {
     const process = require('process');
     window.process = process;
 }
-
-class WalletManager {
-    coinbaseProvider;
-    metamaskProvider;
-    coinbaseChainId;
-    metamaskChainId;
-    CoinbaseConnect;
-    getMetamaskProvider
-
-    constructor() {
-        this.coinbaseProvider = null;
-        this.metamaskProvider = null;
-        this.coinbaseChainId = null;
-        this.metamaskChainId = null;
-
-        const cbProvider = setupCoinbaseWallet()
-        this.coinbaseProvider = cbProvider
-        const metaProvider = this.getMetamaskProvider()
-        this.metamaskProvider = metaProvider
-
-        this.getMetamaskProvider = async () => {
-            if (window.ethereum) {
-                console.log('found window.ethereum>>');
-                return window.ethereum;
-            } else {
-                const provider = createMetaMaskProvider();
-                return provider;
-            }
-        }
-    }
-
-    // async init() {
-    //     this.coinbaseProvider = await setupCoinbaseWallet();
-    //     this.metamaskProvider = await this.getMetamaskProvider();
-    // }
-
-
-    // async CoinbasePersonalSign(messageHash, checkSumAddress) {
-    //     try {
-    //         const signature = await coinbaseProvider.request({
-    //             method: 'personal_sign',
-    //             params: [messageHash, checkSumAddress]
-    //         })
-    //         console.log('Signature:', signature);
-    //         return signature
-    //     } catch (error) {
-    //         console.log("error while connect", error);
-    //     }
-    // }
-
-    // async CoinbaseDisconnect() {
-
-    // }
-
-    // async CoinbasePayment() {
-
-    // }
-
-    // async CoinbaseContractCall() {
-
-    // }
-
-
-    // // METAMASK functionality
-    // async MetamaskConnect() {
-    //     console.log("connectWallet runs....")
-    //     try {
-    //         const [accounts, chainId] = await this.getMetamaskAccounts(metamaskProvider);
-    //         if (accounts && accounts.length > 0 && chainId) {
-    //             const account = getNormalizeAddress(accounts);
-    //             this.metamaskChainId = chainId;
-    //             storage.set('connected', { connected: true, wallet: 'metamask' });
-    //             // metamaskSubscribeToEvents(metamaskProvider)
-    //             return account
-    //         }
-    //     } catch (e) {
-    //         console.log("error while connect", e);
-    //     }
-    // }
-
-    // async MetamaskPersonalSign(messageHash, checkSumAddress) {
-    //     try {
-    //         const signature = await metamaskProvider.request({
-    //             method: 'personal_sign',
-    //             params: [messageHash, checkSumAddress]
-    //         });
-    //         console.log(`Signature: ${signature}`);
-    //         return signature;
-    //     } catch (error) {
-    //         console.error("Failed to sign message with MetaMask:", error);
-    //         throw error;
-    //     }
-    // };
-
-
-    // async MetamaskDisconnect() {
-    //     try {
-    //         console.log("disconnectWallet runs")
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // }
-
-    // async MetamaskPayment() {
-
-    // }
-
-
-    // async MetamaskContractCall() {
-
-    // }
-
-
-    // async getMetamaskAccounts() {
-    //     if (metamaskProvider) {
-    //         const [accounts, chainId] = await Promise.all([
-    //             metamaskProvider.request({
-    //                 method: 'eth_requestAccounts',
-    //             }),
-    //             metamaskProvider.request({ method: 'eth_chainId' }),
-    //         ]);
-    //         return [accounts, chainId];
-    //     }
-    //     return false;
-    // }
-
-    // async metamaskSubscribeToEvents() {
-    //     if (metamaskProvider) {
-    //         // metamaskProvider.on(EthereumEvents.CHAIN_CHANGED, handleChainChanged);
-    //         // metamaskProvider.on(EthereumEvents.ACCOUNTS_CHANGED, handleAccountsChanged);
-    //         // metamaskProvider.on(EthereumEvents.CONNECT, handleConnect);
-    //         // metamaskProvider.on(EthereumEvents.DISCONNECT, handleDisconnect);
-    //     }
-    // }
-
-    // async metamaskUnsubscribeToEvents() {
-    //     if (metamaskProvider) {
-    //         // metamaskProvider.removeListener(EthereumEvents.CHAIN_CHANGED, handleChainChanged);
-    //         // metamaskProvider.removeListener(EthereumEvents.ACCOUNTS_CHANGED, handleAccountsChanged);
-    //         // metamaskProvider.removeListener(EthereumEvents.CONNECT, handleConnect);
-    //         // metamaskProvider.removeListener(EthereumEvents.DISCONNECT, handleDisconnect);
-    //     }
-    // }
-
-    // async connectEagerly() {
-    //     const metamask = await storage.get('connected');
-    //     if (metamask?.connected) {
-    //         await this.MetamaskConnect();
-    //     }
-    // }
-
-    // async metamaskHandleChainChanged(chainId) {
-    //     this.metamaskChainId = chainId;
-    // }
-
-    // async metamaskHandleAccountsChanged(accounts) {
-    //     if (accounts.length > 0) {
-    //         console.log("[account changes]: ", getNormalizeAddress(accounts))
-    //     }
-    // }
-
-}
-
-// Assuming setupCoinbaseWallet and other dependencies are imported correctly
-
 export const createWalletManager = () => {
     const coinbaseProvider = setupCoinbaseWallet();
+    const metamaskProvider = getMetamaskProvider();
+
+    const getMetamaskProvider = async () => {
+        if (window.ethereum) {
+            console.log('found window.ethereum>>');
+            return window.ethereum;
+        } else {
+            const provider = createMetaMaskProvider();
+            return provider;
+        }
+    }
 
     const coinbaseConnect = async () => {
         try {
@@ -201,5 +47,123 @@ export const createWalletManager = () => {
         }
     };
 
-    return { coinbaseConnect };
+    const coinbasePersonalSign = async (messageHash, checkSumAddress) => {
+        try {
+            const signature = await coinbaseProvider.request({
+                method: 'personal_sign',
+                params: [messageHash, checkSumAddress]
+            })
+            console.log('Signature:', signature);
+            return signature
+        } catch (error) {
+            console.log("error while connect", error);
+        }
+    }
+
+    const coinbaseDisconnect = async () => {
+
+    }
+
+    const coinbasePayment = async () => {
+
+    }
+
+    const coinbaseContractCall = async () => {
+
+    }
+
+    const metamaskConnect = async () => {
+        console.log("connectWallet runs....")
+        try {
+            const [accounts, chainId] = await getMetamaskAccounts(metamaskProvider);
+            if (accounts && accounts.length > 0 && chainId) {
+                const account = getNormalizeAddress(accounts);
+                storage.set('connected', { connected: true, wallet: 'metamask' });
+                // metamaskSubscribeToEvents(metamaskProvider)
+                return { account, chainId }
+            }
+        } catch (e) {
+            console.log("error while connect", e);
+        }
+    }
+
+    const metamaskPersonalSign = async (messageHash, checkSumAddress) => {
+        try {
+            const signature = await metamaskProvider.request({
+                method: 'personal_sign',
+                params: [messageHash, checkSumAddress]
+            });
+            console.log(`Signature: ${signature}`);
+            return signature;
+        } catch (error) {
+            console.error("Failed to sign message with MetaMask:", error);
+            throw error;
+        }
+    };
+
+    const MetamaskDisconnect = () => {
+        try {
+            console.log("disconnectWallet runs")
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const MetamaskPayment = () => {
+
+    }
+
+    const MetamaskContractCall = () => {
+
+    }
+
+    const getMetamaskAccounts = async () => {
+        if (metamaskProvider) {
+            const [accounts, chainId] = await Promise.all([
+                metamaskProvider.request({
+                    method: 'eth_requestAccounts',
+                }),
+                metamaskProvider.request({ method: 'eth_chainId' }),
+            ]);
+            return [accounts, chainId];
+        }
+        return false;
+    }
+
+    const metamaskSubscribeToEvents = () => {
+        if (metamaskProvider) {
+            // metamaskProvider.on(EthereumEvents.CHAIN_CHANGED, handleChainChanged);
+            // metamaskProvider.on(EthereumEvents.ACCOUNTS_CHANGED, handleAccountsChanged);
+            // metamaskProvider.on(EthereumEvents.CONNECT, handleConnect);
+            // metamaskProvider.on(EthereumEvents.DISCONNECT, handleDisconnect);
+        }
+    }
+
+    const metamaskUnsubscribeToEvents = () => {
+        if (metamaskProvider) {
+            // metamaskProvider.removeListener(EthereumEvents.CHAIN_CHANGED, handleChainChanged);
+            // metamaskProvider.removeListener(EthereumEvents.ACCOUNTS_CHANGED, handleAccountsChanged);
+            // metamaskProvider.removeListener(EthereumEvents.CONNECT, handleConnect);
+            // metamaskProvider.removeListener(EthereumEvents.DISCONNECT, handleDisconnect);
+        }
+    }
+
+    const connectEagerly = async () => {
+        const metamask = await storage.get('connected');
+        if (metamask?.connected) {
+            await this.MetamaskConnect();
+        }
+    }
+
+    const metamaskHandleChainChanged = (chainId) => {
+        this.metamaskChainId = chainId;
+    }
+
+    const metamaskHandleAccountsChanged = (accounts) => {
+        if (accounts.length > 0) {
+            console.log("[account changes]: ", getNormalizeAddress(accounts))
+        }
+    }
+
+    return { coinbaseConnect, getMetamaskProvider, coinbasePersonalSign, metamaskConnect, metamaskPersonalSign };
 };
